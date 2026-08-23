e43a8ce4d63183301976c78a1171684fb3e7586e
angha singh <anghasingh@anghas-MacBook-Air.local>
2026-08-23 22:05:08 +0530

    add linear search algorithm

---
+++ linearsearch.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+using namespace std;
+int main(){
+     int n,i,s;
+     cout<<"enter the size of array"<<endl;
+     cin>>n;
+     int arr[n];
+     cout<<"enter elements"<<endl;        //linear search
+     for(i=0;i<n;i++){
+          
+cin>>arr[i];
+     }
+
+cout<<"enter the element to search"<<endl;
+cin>>s;
+for(i=0;i<n;i++){
+     if(arr[i]==s){
+     cout<<"the element found at index= "<<i<<endl; 
+     return 0;
+     }
+}
+cout<<"element not found "<<endl;
+return 0;
+
+}
