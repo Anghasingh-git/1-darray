6cced0f437165f7d0516212c40a16856780890d0
angha singh <anghasingh@anghas-MacBook-Air.local>
2026-08-23 22:22:10 +0530

    add deletion

---
+++ deletion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, pos;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter position to delete (1 to " << n << "): ";
+    cin >> pos;
+    for (int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    n--; 
+    cout << "Array after deletion: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
